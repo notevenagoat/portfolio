aef136656d25284b087d45c28e54f63686284bf1
notevenagoat <itinoco@pacificstream.com.au>
2021-08-03 01:59:37 +1000

    button shows headers then dissapears

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import Introeffect from "./Introeffect.js";
+import React, { useState } from 'react';
+
 
 function App() {
+  const [visible, setVisible] = useState(false);
   return (
-    <div>
-
+    <div class="container">
+      <div class="box header">
+      {visible ?<div class="top titleA" > who am I </div>:""}
+      {visible ?<div class="top titleB" > projects </div>:""}
+      {visible ?<div class="top titleC" > curriculum vitae </div>:""}
+       </div>
+    
+      <div class="box main">
+      {!visible ?<Introeffect setVisible={setVisible} visible={visible}/>:""}
+ 
+      </div>
+      <div class="box footer">
+      <p> contact </p>
+      </div>
+    
     </div>
   );
 }
---
+++ src/Introeffect.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+
+export function Introeffect(props) {
+
+  const { setVisible, visible} = props;
+
+
+  const handleClicked = () => {
+     setVisible(!visible);
+   
+  }
+
+  return (
+    <div>
+       <div id="intro-effect-box" class="intro-effect" onClick={handleClicked}>
+        <p> notevenagoat / portfolio </p>
+      </div>
+    </div>
+  );
+}
+
+export default Introeffect;
